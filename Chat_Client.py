fa217b0c439bc4173676595e9d2edb36187f5c03
meenutresa <meenuvincent1992@gmail.com>
2017-10-23 03:03:06 +0100

    chat room where mutiple clients can chat.

--- Chat_Client.py
+++ Chat_Client.py
@@ -1,23 +1,67 @@
 import socket
 import sys
+import threading
+from threading import Thread
+
+class Server_Thread(Thread):
+    def __init__(self,socket):
+        Thread.__init__(self)
+        self.socket = socket
+
+    def run(self):
+        while True:
+            msg_to_client = input("Enter the message : ")
+            self.socket.send(msg_to_client.encode())
+            msg_from_server = self.socket.recv(buff_size).decode()
+            if "Bye" in msg_from_server:
+                #msg_from_server = self.socket.recv(buff_size).decode()
+                #print("<Me> : " + msg_from_server)
+                self.socket.close()
+                sys.exit()
+            else:
+                print("<Me> :  " + msg_from_server)
+
+
+class Server_Broadcast_Thread(Thread):
+    def __init__(self,socket):
+        Thread.__init__(self)
+        self.socket = socket
+
+    def run(self):
+        socket2 =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket2.connect((ip, port2))
+        while True:
+            message = socket2.recv(buff_size).decode()
+            print("\n" + message)
+
 
 buff_size = 2048
 ip = sys.argv[1]
 port = int(sys.argv[2])
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((ip, port))
+port2 = 125
+server_threads = []
+socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket1.connect((ip, port))
 #COnnected to server
-msg_from_server=socket.recv(buff_size).decode()
+#socket2 =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#socket2.connect((ip, port2))
 
+msg_from_server=socket1.recv(buff_size).decode()
 print("Message from Server is : " + msg_from_server)
 
+server_thread = Server_Thread(socket1)
+server_thread.daemon = True
+server_thread.start()
+server_threads.append(server_thread)
+
+server_broadcast_thread = Server_Broadcast_Thread(socket1)
+server_broadcast_thread.daemon = True
+server_broadcast_thread.start()
+server_threads.append(server_broadcast_thread)
+
 while True:
-    msg_to_client = input("Enter the message : ")
-    socket.send(msg_to_client.encode())
-    msg_from_server = socket.recv(buff_size).decode()
-    print("Message from server : " + msg_from_server)
-    if "Bye" in msg_from_server:
-        msg_from_server = socket.recv(buff_size).decode()
-        print("Message from server : " + msg_from_server)
-        socket.close()
-        sys.exit()
+    for st in server_threads:
+        st.join(600)
+        if not st.isAlive():
+            break
+    break
--- Chat_Server.py
+++ Chat_Server.py
@@ -3,7 +3,6 @@ import sys
 import queue
 import threading
 from threading import Thread
-from socketserver import ThreadingMixIn
 
 no_of_clients_connected = 0
 
@@ -23,24 +22,67 @@ class Client_Thread(Thread):
         #if no_of_clients_connected == 1:
         while True:
             msg_from_client=self.socket.recv(buff_size).decode()
-            message = msg_from_client
-            self.socket.send(message.encode())
-            print("from thread no : of threads : " + str(no_of_clients_connected))
+
             if "Bye" in msg_from_client:
-                message = "Thank You for Joining!!!"
-                self.socket.send(message.encode())
+                message = username + "left from chat!!!"
+                lock.acquire()
+                del send_queues[self.socket.fileno()]
+                for i, j in zip(send_queues.values(), send_queues):
+                    if j == self.socket.fileno():
+                        pass
+                    else:
+                        i.put(message)
+                lock.release()
+                msg = "Bye"
+                self.socket.send(msg.encode())
                 break;
+            else:
+                message = msg_from_client
+                msg = username + " : " + message
+                lock.acquire()
+                for i, j in zip(send_queues.values(), send_queues):
+                    if j == self.socket.fileno():
+                        pass
+                    else:
+                        i.put(msg)
+                lock.release()
+                self.socket.send(message.encode())
+                #print("from thread no : of threads : " + str(no_of_clients_connected))
 
 
+class Client_Broadcast_Thread(Thread):
+    def __init__(self,socket):
+        Thread.__init__(self)
+        self.socket = socket
+        print ("New Client Broadcast Thread started")
+
+    def run(self):
+        tcp_socket2.listen(1)
+        (client_soc1,addr) = tcp_socket2.accept()
+        while True:
+            try:
+                message = send_queues[self.socket.fileno()].get(False)
+                #print("Message in Broadcast class : " + message)
+                client_soc1.send(message.encode())
+            except queue.Empty:
+                message = "No message to broadcast"
+            except KeyError as e:
+                pass
 
 buff_size = 2048
 lock = threading.Lock()
 send_queues = {}
 ip = '0.0.0.0'
 port = int(sys.argv[1])
+port2 = 125
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 tcp_socket.bind(('',port))
+
+tcp_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_socket2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcp_socket2.bind(('', port2))
+
 client_threads = []
 while True:
     tcp_socket.listen(6)
@@ -64,19 +106,11 @@ while True:
     client_thread.start()
     client_threads.append(client_thread)
 
-    '''
-    message = "Hello Client"
-    client_soc.send(message.encode())
+    client_broadcast_thread = Client_Broadcast_Thread(client_soc)
+    client_broadcast_thread.daemon = True
+    client_broadcast_thread.start()
+    client_threads.append(client_broadcast_thread)
 
-    msg_from_client=client_soc.recv(buff_size).decode()
-    message = msg_from_client
-    client_soc.send(message.encode())
-    if "Bye" in msg_from_client:
-        message = "Thank You for Joining!!!"
-        client_soc.send(message.encode())
-        tcp_socket.close()
-        sys.exit()
-        '''
 for ct in client_threads:
     ct.join()
     print("Reached the END")
