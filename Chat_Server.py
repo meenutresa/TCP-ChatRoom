ace8653b9ba069aaec12d757d0804a3358f9d6e7
meenutresa <meenuvincent1992@gmail.com>
2017-11-20 19:23:17 +0000

    test

--- Chat_Server.py
+++ Chat_Server.py
@@ -169,6 +169,9 @@ class Client_Thread(Thread):
         #self.socket.send(message.encode())
         msg_from_client=self.socket.recv(buff_size).decode()
         if "HELO" in msg_from_client:
+            host_name = socket.gethostname()
+            host_ip = socket.gethostbyname(host_name)
+            host_port = port
             message = str(msg_from_client)+"IP:"+str(host_ip)+"\nPort:"+str(host_port)+"\nStudentID:17312351\n"
             self.socket.send(message.encode())
         username = "<" + client_ip + "," + str(client_port) + ">"
