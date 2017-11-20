cbc8643274fc5841a55794322ca76ca6ef82eb6f
meenutresa <meenuvincent1992@gmail.com>
2017-11-20 18:54:30 +0000

    test

--- Chat_Server.py
+++ Chat_Server.py
@@ -400,7 +400,7 @@ port2 = 5000
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 tcp_socket.bind(('',port))
-tcp_socket.listen(6)
+tcp_socket.listen(5)
 
 #tcp_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #tcp_socket2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
