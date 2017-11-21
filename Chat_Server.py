e2043c0aa0bfda040eb60fef3dd9dcae4d9e14af
meenutresa <meenuvincent1992@gmail.com>
2017-11-21 01:48:18 +0000

    try adition

--- Chat_Server.py
+++ Chat_Server.py
@@ -181,7 +181,10 @@ class Client_Thread(Thread):
         #print("Message from Client : " +username+ ":" + msg_from_client)
         #--------------------------------------------------
         while True:
-            msg_from_client=self.socket.recv(buff_size).decode()
+            try:
+                msg_from_client=self.socket.recv(buff_size).decode()
+            except:
+                pass
             #if flag!=1:
             #    print("Client_Threads",client_threads)
             #    print("Client_Threads",self.client_nam)
